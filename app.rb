1318f4dfe2c0f9b39ebfe1d33ece9c7714b6580a
Tomas Dinkov <tomas.dinkov@gmail.com>
2017-08-13 21:08:07 +0200

    first model class implemented

--- app.rb
+++ app.rb
@@ -3,10 +3,15 @@ require_relative 'model'
 
 get '/' do
   # use the views/index.erb file
+  @measurements = Measurement.all
   erb :index
 end
 
 get '/agent' do
   # use the views/agent.erb file
   erb :agent
+end
+
+post '/' do
+
 end
