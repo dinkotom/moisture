ac353525ad2a1d38315360478c91cd743cf1e23e
Tomas Dinkov <tomas.dinkov@gmail.com>
2017-08-13 20:53:27 +0200

    require changed to require_relative

--- model.rb
+++ model.rb
@@ -2,3 +2,15 @@ $logger = Logger.new('application.log')
 DataMapper::Logger.new('sql.log', :debug)
 DataMapper::setup(:default, 'mysql://adminQcdfxJq:wCHYWLl3YEa4@127.3.119.2:3306/moisture')
 # DataMapper::setup(:default, 'mysql://bezplenky:SalvatorDali01@localhost/bezplenky')
+
+# encoding: UTF-8
+
+class Measurement
+  include DataMapper::Resource
+  property :id, Serial
+  property :time, DateTime
+  property :temperature, Float
+  property :relative_humidity, Float
+  property :dew_point, Float
+  property :comment, Text
+end
