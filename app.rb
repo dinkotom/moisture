6c31a87c5601598678c61860f5cd6fcea4ec4e5d
Tomas Dinkov <tomas.dinkov@gmail.com>
2017-08-15 20:42:09 +0200

    post method for saving implemented

--- app.rb
+++ app.rb
@@ -13,5 +13,16 @@ get '/agent' do
 end
 
 post '/' do
+  temperature = params['temperature']
+  relative_humidity = params['relative_humidity']
+  dew_point = params['dew_point']
+  comment = params['comment']
 
+  Measurement.create(
+      :time => DateTime.now,
+      :temperature => temperature,
+      :relative_humidity => relative_humidity,
+      :dew_point => dew_point,
+      :comment => comment
+  )
 end
