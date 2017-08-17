3347e6311321eaf0a93577da345a251e7295776c
Tomas Dinkov <tomas.dinkov@gmail.com>
2017-08-17 08:21:51 +0200

    fixed defect in view

--- model.rb
+++ model.rb
@@ -1,7 +1,7 @@
 $logger = Logger.new('application.log')
 DataMapper::Logger.new('sql.log', :debug)
-# DataMapper::setup(:default, 'mysql://adminQcdfxJq:wCHYWLl3YEa4@127.3.119.2:3306/moisture')
-DataMapper::setup(:default, 'mysql://moisture:SalvatorDali01@localhost/moisture')
+DataMapper::setup(:default, 'mysql://adminQcdfxJq:wCHYWLl3YEa4@127.3.119.2:3306/moisture')
+# DataMapper::setup(:default, 'mysql://moisture:SalvatorDali01@localhost/moisture')
 
 # encoding: UTF-8
 
